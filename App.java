64e948ba827c4dd3f39522dfcaa35ea244d48621
akmal5120 <48241656+akmal5120@users.noreply.github.com>
2021-07-18 18:28:33 +0530

    Update App.java

--- App.java
+++ App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Akmal Jenkins compile !" );
+        System.out.println( "Hello Akmal Jenkins compile it !" );
     }
 }
